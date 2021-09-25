e79bd5467d0932b4508a5b43f26867ec7224ea1c
Armen <armen@digidez.com>
2021-09-25 22:08:57 +0200

    31. New Design Markup (7)

--- www/views/myquote/partials/quote-results-form.php
+++ www/views/myquote/partials/quote-results-form.php
@@ -1,245 +1,269 @@
-<div class="quotes-result__container results-table">
-	<div class="quotes-result__containerHeader">
-		<h2 class="quotes-result__containerTitle heading-5">No Medical Exam Policies</h2>
-		<div class="heading-6">Total: <span>2</span></div>
-	</div>
-	<div class="quotes-result__tableHeader tableHeader">
-		<div class="tableHeader__col fontBodyS">Insurance Company</div>
-		<div class="tableHeader__col fontBodyS">Monthly Payment</div>
-		<div class="tableHeader__col fontBodyS">Financial Rating</div>
-		<div class="tableHeader__col fontBodyS">Term Length</div>
-	</div>
-	<hr>
-	
-	<div class="quotes-result__tableRow tableRow">
-		<div class="tableRow__col">
-			<div class="tableRow__imgWrapp">
-				<img src="/v2/myquote/img/quote-result-logo-1.png" alt="logo" class="tableRow__img">
-				<div class="tableRow__starsWrapp">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star-no-active.png" alt="*">
-				</div>
-			</div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__price fontBodyM">$46.55</div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__rating fontBodyM">A <span>(Excellent)</span></div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__term fontBodyM">10 Year Plan</div>
-		</div>
-		<div class="tableRow__col tableRow__col--hiden">
-			<div class="tableRow__box">
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Monthly Payment</span>
-					<span class="fontBodyM">$46.55</span>
-				</div>
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Financial Rating</span>
-					<span class="fontBodyM">A <span>(Excellent)</span></span>
-				</div>
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Term Length</span>
-					<span class="fontBodyM">10 Year Plan</span>
-				</div>
-			</div>
-		</div>
-		<div class="tableRow__col">
-			<button type="submit" class="tableRow__btn main-btn button-big">apply now</button>
-		</div>
-	</div>
-	<hr>
-	
-	<div class="quotes-result__tableRow tableRow">
-		<div class="tableRow__col">
-			<div class="tableRow__imgWrapp">
-				<img src="/v2/myquote/img/quote-result-logo-4.png" alt="logo" class="tableRow__img">
-				<div class="tableRow__starsWrapp">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star-no-active.png" alt="*">
-				</div>
-			</div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__price fontBodyM">$46.55</div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__rating fontBodyM">A <span>(Excellent)</span></div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__term fontBodyM">10 Year Plan</div>
-		</div>
-		<div class="tableRow__col tableRow__col--hiden">
-			<div class="tableRow__box">
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Monthly Payment</span>
-					<span class="fontBodyM">$46.55</span>
-				</div>
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Financial Rating</span>
-					<span class="fontBodyM">A <span>(Excellent)</span></span>
-				</div>
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Term Length</span>
-					<span class="fontBodyM">10 Year Plan</span>
-				</div>
-			</div>
-		</div>
-		<div class="tableRow__col">
-			<button type="submit" class="tableRow__btn main-btn button-big">apply now</button>
-		</div>
-	</div>
-</div>
+<?php if($total_plans_count > 0):?>
 
-<div class="quotes-result__container results-table">
-	<div class="quotes-result__containerHeader">
-		<h2 class="quotes-result__containerTitle heading-5">Policies that Offer Accelerated Underwriting</h2>
-		<div class="heading-6">Total: 3</div>
+	<?php if(count($prices['plans']['exam_no'])):?>
+	<div class="quotes-result__container results-table">
+		<div class="quotes-result__containerHeader">
+			<h2 class="quotes-result__containerTitle heading-5">No Medical Exam Policies</h2>
+			<div class="heading-6">Total: <span>2</span></div>
+		</div>
+		<div class="quotes-result__tableHeader tableHeader">
+			<div class="tableHeader__col fontBodyS">Insurance Company</div>
+			<div class="tableHeader__col fontBodyS">Monthly Payment</div>
+			<div class="tableHeader__col fontBodyS">Financial Rating</div>
+			<div class="tableHeader__col fontBodyS">Term Length</div>
+		</div>
+		<hr>
+		
+		<div class="quotes-result__tableRow tableRow">
+			<div class="tableRow__col">
+				<div class="tableRow__imgWrapp">
+					<img src="/v2/myquote/img/quote-result-logo-1.png" alt="logo" class="tableRow__img">
+					<div class="tableRow__starsWrapp">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star-no-active.png" alt="*">
+					</div>
+				</div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__price fontBodyM">$46.55</div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__rating fontBodyM">A <span>(Excellent)</span></div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__term fontBodyM">10 Year Plan</div>
+			</div>
+			<div class="tableRow__col tableRow__col--hiden">
+				<div class="tableRow__box">
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Monthly Payment</span>
+						<span class="fontBodyM">$46.55</span>
+					</div>
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Financial Rating</span>
+						<span class="fontBodyM">A <span>(Excellent)</span></span>
+					</div>
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Term Length</span>
+						<span class="fontBodyM">10 Year Plan</span>
+					</div>
+				</div>
+			</div>
+			<div class="tableRow__col">
+				<button type="submit" class="tableRow__btn main-btn button-big">apply now</button>
+			</div>
+		</div>
+		<hr>
+		
+		<div class="quotes-result__tableRow tableRow">
+			<div class="tableRow__col">
+				<div class="tableRow__imgWrapp">
+					<img src="/v2/myquote/img/quote-result-logo-4.png" alt="logo" class="tableRow__img">
+					<div class="tableRow__starsWrapp">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star-no-active.png" alt="*">
+					</div>
+				</div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__price fontBodyM">$46.55</div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__rating fontBodyM">A <span>(Excellent)</span></div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__term fontBodyM">10 Year Plan</div>
+			</div>
+			<div class="tableRow__col tableRow__col--hiden">
+				<div class="tableRow__box">
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Monthly Payment</span>
+						<span class="fontBodyM">$46.55</span>
+					</div>
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Financial Rating</span>
+						<span class="fontBodyM">A <span>(Excellent)</span></span>
+					</div>
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Term Length</span>
+						<span class="fontBodyM">10 Year Plan</span>
+					</div>
+				</div>
+			</div>
+			<div class="tableRow__col">
+				<button type="submit" class="tableRow__btn main-btn button-big">apply now</button>
+			</div>
+		</div>
 	</div>
-	<div class="quotes-result__tableHeader tableHeader">
-		<div class="tableHeader__col fontBodyS">Insurance Company</div>
-		<div class="tableHeader__col fontBodyS">Monthly Payment</div>
-		<div class="tableHeader__col fontBodyS">Financial Rating</div>
-		<div class="tableHeader__col fontBodyS">Term Length</div>
-	</div>
-	<hr>
+	<?php endif;?>
 	
-	<div class="quotes-result__tableRow tableRow">
-		<div class="tableRow__col">
-			<div class="tableRow__imgWrapp">
-				<img src="/v2/myquote/img/quote-result-logo-3.png" alt="logo" class="tableRow__img">
-				<div class="tableRow__starsWrapp">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star-no-active.png" alt="*">
+	<?php if(count($prices['plans']['exam_yes'])):?>
+	<div class="quotes-result__container results-table">
+		<div class="quotes-result__containerHeader">
+			<h2 class="quotes-result__containerTitle heading-5">Policies that Offer Accelerated Underwriting</h2>
+			<div class="heading-6">Total: 3</div>
+		</div>
+		<div class="quotes-result__tableHeader tableHeader">
+			<div class="tableHeader__col fontBodyS">Insurance Company</div>
+			<div class="tableHeader__col fontBodyS">Monthly Payment</div>
+			<div class="tableHeader__col fontBodyS">Financial Rating</div>
+			<div class="tableHeader__col fontBodyS">Term Length</div>
+		</div>
+		<hr>
+		
+		<div class="quotes-result__tableRow tableRow">
+			<div class="tableRow__col">
+				<div class="tableRow__imgWrapp">
+					<img src="/v2/myquote/img/quote-result-logo-3.png" alt="logo" class="tableRow__img">
+					<div class="tableRow__starsWrapp">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star-no-active.png" alt="*">
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__price fontBodyM">$46.55</div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__rating fontBodyM">A <span>(Excellent)</span></div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__term fontBodyM">10 Year Plan</div>
-		</div>
-		<div class="tableRow__col tableRow__col--hiden">
-			<div class="tableRow__box">
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Monthly Payment</span>
-					<span class="fontBodyM">$46.55</span>
-				</div>
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Financial Rating</span>
-					<span class="fontBodyM">A <span>(Excellent)</span></span>
-				</div>
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Term Length</span>
-					<span class="fontBodyM">10 Year Plan</span>
+			<div class="tableRow__col">
+				<div class="tableRow__price fontBodyM">$46.55</div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__rating fontBodyM">A <span>(Excellent)</span></div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__term fontBodyM">10 Year Plan</div>
+			</div>
+			<div class="tableRow__col tableRow__col--hiden">
+				<div class="tableRow__box">
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Monthly Payment</span>
+						<span class="fontBodyM">$46.55</span>
+					</div>
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Financial Rating</span>
+						<span class="fontBodyM">A <span>(Excellent)</span></span>
+					</div>
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Term Length</span>
+						<span class="fontBodyM">10 Year Plan</span>
+					</div>
 				</div>
 			</div>
+			<div class="tableRow__col">
+				<button type="submit" class="tableRow__btn main-btn button-big">apply now</button>
+			</div>
 		</div>
-		<div class="tableRow__col">
-			<button type="submit" class="tableRow__btn main-btn button-big">apply now</button>
+		<hr>
+		
+		<div class="quotes-result__tableRow tableRow">
+			<div class="tableRow__col">
+				<div class="tableRow__imgWrapp">
+					<img src="/v2/myquote/img/quote-result-logo-4.png" alt="logo" class="tableRow__img">
+					<div class="tableRow__starsWrapp">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star-no-active.png" alt="*">
+					</div>
+				</div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__price fontBodyM">$46.55</div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__rating fontBodyM">A <span>(Excellent)</span></div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__term fontBodyM">10 Year Plan</div>
+			</div>
+			<div class="tableRow__col tableRow__col--hiden">
+				<div class="tableRow__box">
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Monthly Payment</span>
+						<span class="fontBodyM">$46.55</span>
+					</div>
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Financial Rating</span>
+						<span class="fontBodyM">A <span>(Excellent)</span></span>
+					</div>
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Term Length</span>
+						<span class="fontBodyM">10 Year Plan</span>
+					</div>
+				</div>
+			</div>
+			<div class="tableRow__col">
+				<button type="submit" class="tableRow__btn main-btn button-big">apply now</button>
+			</div>
+		</div>
+		<hr>
+		
+		<div class="quotes-result__tableRow tableRow">
+			<div class="tableRow__col">
+				<div class="tableRow__imgWrapp">
+					<img src="/v2/myquote/img/quote-result-logo-1.png" alt="logo" class="tableRow__img">
+					<div class="tableRow__starsWrapp">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star.png" alt="*">
+						<img src="/v2/common/images/star-no-active.png" alt="*">
+					</div>
+				</div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__price fontBodyM">$46.55</div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__rating fontBodyM">A <span>(Excellent)</span></div>
+			</div>
+			<div class="tableRow__col">
+				<div class="tableRow__term fontBodyM">10 Year Plan</div>
+			</div>
+			<div class="tableRow__col tableRow__col--hiden">
+				<div class="tableRow__box">
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Monthly Payment</span>
+						<span class="fontBodyM">$46.55</span>
+					</div>
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Financial Rating</span>
+						<span class="fontBodyM">A <span>(Excellent)</span></span>
+					</div>
+					<div class="tableRow__boxRow">
+						<span class="fontBodyS">Term Length</span>
+						<span class="fontBodyM">10 Year Plan</span>
+					</div>
+				</div>
+			</div>
+			<div class="tableRow__col">
+				<button type="submit" class="tableRow__btn main-btn button-big">apply now</button>
+			</div>
 		</div>
 	</div>
-	<hr>
-	
-	<div class="quotes-result__tableRow tableRow">
-		<div class="tableRow__col">
-			<div class="tableRow__imgWrapp">
-				<img src="/v2/myquote/img/quote-result-logo-4.png" alt="logo" class="tableRow__img">
-				<div class="tableRow__starsWrapp">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star-no-active.png" alt="*">
+	<?php endif;?>
+
+<?php else:?>
+	<div class="quotes-result__container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="alert alert-danger">
+					<strong>
+						<?php if(!empty($prices['message'])):?>
+							<?=$prices['message'];?>
+						<?php else:?>
+							Sorry, but we could not find the appropriate plan for you.
+						<?php endif;?>
+					</strong>
 				</div>
 			</div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__price fontBodyM">$46.55</div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__rating fontBodyM">A <span>(Excellent)</span></div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__term fontBodyM">10 Year Plan</div>
-		</div>
-		<div class="tableRow__col tableRow__col--hiden">
-			<div class="tableRow__box">
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Monthly Payment</span>
-					<span class="fontBodyM">$46.55</span>
-				</div>
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Financial Rating</span>
-					<span class="fontBodyM">A <span>(Excellent)</span></span>
-				</div>
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Term Length</span>
-					<span class="fontBodyM">10 Year Plan</span>
-				</div>
-			</div>
-		</div>
-		<div class="tableRow__col">
-			<button type="submit" class="tableRow__btn main-btn button-big">apply now</button>
 		</div>
 	</div>
-	<hr>
-	
-	<div class="quotes-result__tableRow tableRow">
-		<div class="tableRow__col">
-			<div class="tableRow__imgWrapp">
-				<img src="/v2/myquote/img/quote-result-logo-1.png" alt="logo" class="tableRow__img">
-				<div class="tableRow__starsWrapp">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star.png" alt="*">
-					<img src="/v2/common/images/star-no-active.png" alt="*">
-				</div>
-			</div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__price fontBodyM">$46.55</div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__rating fontBodyM">A <span>(Excellent)</span></div>
-		</div>
-		<div class="tableRow__col">
-			<div class="tableRow__term fontBodyM">10 Year Plan</div>
-		</div>
-		<div class="tableRow__col tableRow__col--hiden">
-			<div class="tableRow__box">
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Monthly Payment</span>
-					<span class="fontBodyM">$46.55</span>
-				</div>
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Financial Rating</span>
-					<span class="fontBodyM">A <span>(Excellent)</span></span>
-				</div>
-				<div class="tableRow__boxRow">
-					<span class="fontBodyS">Term Length</span>
-					<span class="fontBodyM">10 Year Plan</span>
-				</div>
-			</div>
-		</div>
-		<div class="tableRow__col">
-			<button type="submit" class="tableRow__btn main-btn button-big">apply now</button>
-		</div>
-	</div>
-</div>
+<?php endif;?>
