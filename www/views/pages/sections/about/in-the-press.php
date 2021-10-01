df8eb2f65f8bc6c384b4040f3fef3284bf7404c5
armenium <armen@digidez.com>
2021-10-01 22:42:45 +0200

    31. New Design Markup (16)

--- www/views/pages/sections/about/in-the-press.php
+++ www/views/pages/sections/about/in-the-press.php
@@ -3,6 +3,7 @@
 		<section class="press">
 			<h1 class="press__tilte heading-2">In the Press</h1>
 			<div class="press__container">
+				
 				<div class="press__item pressItem">
 					<div class="pressItem__header">
 						<div class="pressItem__imgBox"><img src="/v2/pages/img/logo-huffpost.png" alt="logo" class="pressItem__img"></div>
@@ -12,46 +13,75 @@
 						</div>
 					</div>
 					<h4 class="pressItem__title main-title">8 Facts About Life Insurance You 100% Need To Know</h4>
-					<p class="pressItem__content fontBodyS">“Many people end up getting pressured into buying a more expensive whole life policy from a commissioned agent,” said John Holloway, a licensed life insurance agent and co-founder of digital life insurance brokerage NoExam.com.
-						In truth, term life insurance is what 90% of people actually need.</p>
+					<p class="pressItem__content fontBodyS">“Many people end up getting pressured into buying a more expensive whole life policy from a commissioned agent,” said John Holloway, a licensed life insurance agent and co-founder of digital life insurance brokerage NoExam.com. In truth, term life insurance is what 90% of people actually need.</p>
 					<a href="#" class="pressItem__btn fontBodyS button-small">read more</a>
 				</div>
+				
 				<div class="press__item pressItem">
 					<div class="pressItem__header">
 						<div class="pressItem__imgBox"><img src="/v2/pages/img/logo-bi.png" alt="logo" class="pressItem__img"></div>
 						<div class="pressItem__companyInfo">
-							<div class="pressItem__companyName tags">Huffpost</div>
-							<div class="pressItem__date fontBodyS">July 29, 2019</div>
+							<div class="pressItem__companyName tags">Business Insider</div>
+							<div class="pressItem__date fontBodyS">August 17, 2019</div>
 						</div>
 					</div>
-					<h4 class="pressItem__title main-title">8 Facts About Life Insurance You 100% Need To Know</h4>
-					<p class="pressItem__content fontBodyS">“Many people end up getting pressured into buying a more expensive whole life policy from a commissioned agent,” said John Holloway, a licensed life insurance agent and co-founder of digital life insurance brokerage NoExam.com. In truth, term life insurance is what 90% of people actually need.</p>
+					<h4 class="pressItem__title main-title">Life insurance: the younger you are, the cheaper it is</h4>
+					<p class="pressItem__content fontBodyS">An analysis of 80,000 life insurance quotes by Noexam.com revealed that the best time to buy life insurance is in your 30s. Because age and health are two of the most important factors in determining rates, you'll get similar coverage at a lower monthly cost, writes Jonathan Holloway, a life insurance agent and the founder.</p>
 					<a href="#" class="pressItem__btn fontBodyS button-small">read more</a>
 				</div>
+				
 				<div class="press__item pressItem">
 					<div class="pressItem__header">
-						<div class="pressItem__imgBox"><img src="/v2/pages/img/press-logo.png" alt="logo" class="pressItem__img"></div>
+						<div class="pressItem__imgBox"><img src="/v2/pages/img/logo-br.png" alt="logo" class="pressItem__img"></div>
 						<div class="pressItem__companyInfo">
-							<div class="pressItem__companyName tags">Huffpost</div>
-							<div class="pressItem__date fontBodyS">July 29, 2019</div>
+							<div class="pressItem__companyName tags">Bankrate</div>
+							<div class="pressItem__date fontBodyS">May 13, 2019</div>
 						</div>
 					</div>
-					<h4 class="pressItem__title main-title">8 Facts About Life Insurance You 100% Need To Know</h4>
-					<p class="pressItem__content fontBodyS">“Many people end up getting pressured into buying a more expensive whole life policy from a commissioned agent,” said John Holloway, a licensed life insurance agent and co-founder of digital life insurance brokerage NoExam.com. In truth, term life insurance is what 90% of people actually need.</p>
+					<h4 class="pressItem__title main-title">Taxes on life insurance: Here’s when proceeds are taxable</h4>
+					<p class="pressItem__content fontBodyS">“If the payout is paid in installments, the interest that accrues on the payouts is taxable. The death-benefit is not taxable, only the interest on installments,” says Jonathan Holloway, co-founder of NoExam.com, a digital life insurance brokerage.</p>
 					<a href="#" class="pressItem__btn fontBodyS button-small">read more</a>
 				</div>
+				
 				<div class="press__item pressItem">
 					<div class="pressItem__header">
-						<div class="pressItem__imgBox"><img src="/v2/pages/img/press-logo.png" alt="logo" class="pressItem__img"></div>
+						<div class="pressItem__imgBox"><img src="/v2/pages/img/logo-ap.png" alt="logo" class="pressItem__img"></div>
 						<div class="pressItem__companyInfo">
-							<div class="pressItem__companyName tags">Huffpost</div>
-							<div class="pressItem__date fontBodyS">July 29, 2019</div>
+							<div class="pressItem__companyName tags">AP News</div>
+							<div class="pressItem__date fontBodyS">July 25, 2018</div>
 						</div>
 					</div>
-					<h4 class="pressItem__title main-title">8 Facts About Life Insurance You 100% Need To Know</h4>
-					<p class="pressItem__content fontBodyS">“Many people end up getting pressured into buying a more expensive whole life policy from a commissioned agent,” said John Holloway, a licensed life insurance agent and co-founder of digital life insurance brokerage NoExam.com. In truth, term life insurance is what 90% of people actually need.</p>
+					<h4 class="pressItem__title main-title">Mea culpa: A business should own up to customer service flub</h4>
+					<p class="pressItem__content fontBodyS">John Holloway’s insurance brokerage, NoExam.com, was honest with a customer from the get-go: He might not get a life insurance policy because he’d had cancer 10 years earlier. Holloway and his business partner, Jonathan Fritz, searched for an insurer they thought would be a good match.</p>
 					<a href="#" class="pressItem__btn fontBodyS button-small">read more</a>
 				</div>
+				
+				<div class="press__item pressItem">
+					<div class="pressItem__header">
+						<div class="pressItem__imgBox"><img src="/v2/pages/img/logo-sd.png" alt="logo" class="pressItem__img"></div>
+						<div class="pressItem__companyInfo">
+							<div class="pressItem__companyName tags">The Simple Dollar</div>
+							<div class="pressItem__date fontBodyS">April 23, 2019</div>
+						</div>
+					</div>
+					<h4 class="pressItem__title main-title">Nine Signs It’s Time to Drop Your Insurance Company</h4>
+					<p class="pressItem__content fontBodyS">Unexpected and unjustified rate spikes should be a big red flag signaling it’s time to find a new provider, says Jonathan Fritz, who has more than a decade of experience working with property and casualty insurance and co-founded the site NoExam.com.</p>
+					<a href="#" class="pressItem__btn fontBodyS button-small">read more</a>
+				</div>
+				
+				<div class="press__item pressItem">
+					<div class="pressItem__header">
+						<div class="pressItem__imgBox"><img src="/v2/pages/img/logo-cheapism.png" alt="logo" class="pressItem__img"></div>
+						<div class="pressItem__companyInfo">
+							<div class="pressItem__companyName tags">Cheapism</div>
+							<div class="pressItem__date fontBodyS">August 1, 2019</div>
+						</div>
+					</div>
+					<h4 class="pressItem__title main-title">32 Ways to Save on Life Insurance</h4>
+					<p class="pressItem__content fontBodyS">Another way to save on life insurance is to purchase multiple policies, a practice that is called "laddering." For example, if you need $1 million of coverage at age 30 but only $500,000 at 40 and $200,000 at 50, then buy a 10-year-term $500,000 policy, a 20-year $300,000 policy, and a 30-year term $200,000 policy..</p>
+					<a href="#" class="pressItem__btn fontBodyS button-small">read more</a>
+				</div>
+				
 			</div>
 		</section>
 	</div>
