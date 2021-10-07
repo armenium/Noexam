d220c350ccc3d33ab5335151e38793533815540a
stas <stas.sklyar.dev@gmail.com>
2021-10-07 21:57:15 +0300

    created life-insurance v2 page

---
+++ frontend/life-insurance-v2/life-insurance-v2.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('.reviews__body').slick({
+        infinite: true,
+        dots: true,
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+            }
+        }, ]
+    });
+});
+
+let questionsItems = [...document.querySelectorAll('.questions__item')].forEach(item => {
+    item.addEventListener('click', (e) => {
+        item.classList.toggle("questions__item--active")
+    });
+});
