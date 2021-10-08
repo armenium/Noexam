7bd1645fa2f448ab4fd82943b845f0d289dc45a8
stas <stas.sklyar.dev@gmail.com>
2021-10-08 20:47:20 +0300

    life insurance page

--- frontend/life-insurance-v2/life-insurance-v2.js
+++ frontend/life-insurance-v2/life-insurance-v2.js
@@ -23,3 +23,31 @@ let questionsItems = [...document.querySelectorAll('.questions__item')].forEach(
 $(function() {
     $('.js_selectpicker').selectpicker();
 });
+
+$(document).ready(function() {
+    $('.life-insurance__cards').slick({
+        infinite: false,
+        dots: false,
+        arrows: false,
+        slidesToScroll: 1,
+        draggable: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    draggable: true,
+                    dots: true,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    draggable: true,
+                    dots: true,
+                }
+            }
+
+        ]
+    });
+});
