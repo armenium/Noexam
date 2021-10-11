5c4b121a838633aa3a107fdad0cacd27ab371f75
stas <stas.sklyar.dev@gmail.com>
2021-10-11 20:04:41 +0300

    created get your rate popup

---
+++ frontend/get-your-rate-pop-up/get-your-rate-popup.js
@@ -0,0 +1,21 @@
+let closeBtn = document.querySelector(".get-your-rate-popup__close-icon")
+let popup = document.querySelector(".get-your-rate-popup")
+let html = document.querySelector("html")
+html.style.overflow = "hidden" // add to open popup function
+
+closeBtn.addEventListener("click", () => {
+    popup.classList.remove("get-your-rate-popup--active")
+    html.style.overflow = "visible"
+})
+
+const marks = ['100k', '150k', '200k', '250k', '300k', '400k', '500k', '600k', '700k', '800k', '900k', '1m'];
+
+$(".get-your-rate-popup__range-slider").ionRangeSlider({
+    skin: "round",
+    values: marks,
+    grid: true,
+    from: 5,
+    prefix: "$",
+    hide_min_max: true,
+    step: 50000
+});
